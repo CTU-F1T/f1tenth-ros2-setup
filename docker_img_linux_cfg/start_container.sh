a93c0c8e7386cd920be5606fdd1453f0e7e86680
Michaela Cihlarova <cihlami1@fel.cvut.cz>
2023-03-14 18:43:42 +0100

    Disabled automatic start of tmux, updated README and created start_container.sh (not tested)
    TODO: update README - create custom overlay
          change f1test repo to redmine

---
+++ docker_img_linux_cfg/start_container.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.tmuxinator
+cp -r ~/f1tenth-ros2-setup/docker_img_linux_cfg/appconfig/tmux ~/.tmuxinator/
+
+docker run -it -d \
+--network=host \
+--restart always \
+-h tx2-ros2-docker \
+--name tx2-ros2-cfg \
+--device=/dev/tty.imu \
+--device=/dev/tty.vesc \
+--device=/dev/tty.teensy \
+-v /home/nvidia/.tmuxinator:/home/nvidia/.tmuxinator \
+-v /home/nvidia/f1tenth:/home/nvidia/f1tenth \
+ctu-iig/tx2-ros2-docker-cfg:latest
